bd10f4803f7d93c0707352ab74d6d7959f6f3607
arinasilchenko <silchenkoarisha@gmail.com>
2026-06-04 23:06:18 +0300

    улучшена читаемость кода

--- ImagesList/ImagesListViewController.swift
+++ ImagesList/ImagesListViewController.swift
@@ -1,14 +1,50 @@
 import UIKit
 
+final class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+
+    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        let imageName = photosName[indexPath.row]
+        guard let image = UIImage(named: imageName) else {
+            return
+        }
+        cell.imageOfCell.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        if indexPath.row % 2 == 0 {
+            cell.likeButton.setImage(UIImage(named: "unactive_like"), for: .normal)
+        } else {
+            cell.likeButton.setImage(UIImage(named: "active_like"), for: .normal)
+        }
+    }
+}
+
 extension ImagesListViewController: UITableViewDelegate {
     func tableView( _ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        // TODO
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else { return 200 }
+        guard image.size.width != 0 else { return 200 }
+        //коэффициент масштабирования = (ширина экрана БЕЗ отступов по 16pt слева и справа) / ширину картинки
         let scale = (tableView.bounds.width - 32) / image.size.width
         let imageHeight = image.size.height * scale
+        //высота картинки с учетом отступов снизу и сверху по 4pt
         return imageHeight + 8
     }
 }
@@ -27,39 +63,4 @@ extension ImagesListViewController: UITableViewDataSource {
         configCell(for: imageListCell, with: indexPath)
         return imageListCell
     }
-    
 }
-
-class ImagesListViewController: UIViewController {
-    
-    @IBOutlet private var tableView: UITableView!
-    
-    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-    
-    private lazy var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .long
-        formatter.timeStyle = .none
-        return formatter
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-    }
-
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        let imageName = photosName[indexPath.row]
-        guard let image = UIImage(named: imageName) else {
-            return
-        }
-        cell.imageOfCell.image = image
-        cell.dateLabel.text = dateFormatter.string(from: Date())
-        if indexPath.row % 2 == 0 {
-            cell.likeButton.setImage(UIImage(named: "unactive_like"), for: .normal)
-        } else {
-            cell.likeButton.setImage(UIImage(named: "active_like"), for: .normal)
-        }
-    }
-}
-
