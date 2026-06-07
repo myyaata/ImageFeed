4a02a6373b3bfe9d982d6e6c8d93dbca1d12a165
arinasilchenko <silchenkoarisha@gmail.com>
2026-06-07 20:41:10 +0300

    сверстан экран профиля

---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var profilePick: UIImageView!
+    @IBOutlet private var logoutButton: UIButton!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+}
